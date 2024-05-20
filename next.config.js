2213d6726d19cdde78d26e550eb92cc323e19999
hakeemsyd <hakeemsyd@gmail.com>
2024-05-20 19:13:31 +0500

    test

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ module.exports = {
   output: 'standalone',
   experimental: {
     outputFileTracingExcludes: {
-        "*": [
-            "node_modules/@swc/core-linux-x64-gnu",
-            "node_modules/@swc/core-linux-x64-musl",
-            "node_modules/@esbuild/linux-x64",
-        ],
+        "*": [],
         },
     },
   rewrites: async () => {
